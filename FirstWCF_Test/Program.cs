6a50b6b1e40c7f55d837b7a48780791a819452ab
CristiMark-PC\Cristi Mark <cristiankeisz@hotmail.com>
2018-11-22 20:54:21 +0200

    Add a new WCF Service App

--- FirstWCF_Test/Program.cs
+++ FirstWCF_Test/Program.cs
@@ -10,8 +10,9 @@ namespace FirstWCF_Test
     {
         static void Main(string[] args)
         {
-            ServiceWCF.Service1Client proxy = new ServiceWCF.Service1Client();
-            Console.WriteLine(proxy.Sum(5, 5));
+           // ServiceWCF.Service1Client proxy = new ServiceWCF.Service1Client();
+            ServiceWCFapp.Service1Client proxyApp = new ServiceWCFapp.Service1Client();
+            Console.WriteLine(proxyApp.Sum(5, 5));
             Console.ReadKey();
         }
     }
